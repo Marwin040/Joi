7a8603bdc3f894b4f8fc7857b71f8a4b1be60813
Marwin040 <markusmarwin00@gmail.com>
2024-11-02 08:48:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,9 +4,9 @@ LOAD = getenv("LOAD", "").split()
 
 NO_LOAD = getenv("NO_LOAD", "").split()
 
-TOKEN = getenv("TOKEN", "6701285365:AAEKn6vbsY6D9uFYqYzoE7BO-Wefj-qjtQM")
+TOKEN = getenv("TOKEN", "7343734756:AAHiKuQRQhY60nl-p7OqskcTlhcBvJIyyeg")
 
 MONGO_DB_URL = getenv(
     "MONGO_DB_URL",
-    "mongodb+srv://moni:<password>@cluster0.i3djv5s.mongodb.net/?retryWrites=true&w=majority",
+    "marwin0985:BEwJvxaADStDLScc@cluster0.oh0nk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
 )
