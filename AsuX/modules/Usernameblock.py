bb2ad67b42cd9a0d4c346bf14feccdf8bd7110e6
Marwin040 <markusmarwin00@gmail.com>
2024-11-03 00:48:37 +0530

    Update Usernameblock.py

--- AsuX/modules/Usernameblock.py
+++ AsuX/modules/Usernameblock.py
@@ -16,26 +16,35 @@ async def stop_chat_username(update: Update, context: ContextTypes.DEFAULT_TYPE)
     chat = update.effective_chat
     message = update.effective_message
     users = update.effective_user
+    
+    # Check if message.text is a string
+    if not isinstance(message.text, str):
+        return  # Exit if message.text is not a valid string
+
     links = re.findall(r"@[^\s]+", message.text)
     if not links:
         return
-    chat_admins = await rani.bot.getChatAdministrators(chat.id)
+    
+    chat_admins = await bot.getChatAdministrators(chat.id)
     admin_list = [x.user.id for x in chat_admins]
+    
     if users.id in admin_list:
         return
+    
     if (await chat.get_member(bot.id)).can_delete_messages:
-        if message.text:
-            for link in links:
-                try:
-                    user = await bot.get_chat(link)
-                    print(user.id)
-                    if len(str(user.id)) > 12:
-                        await message.reply_text(
-                            f"{users.first_name}, ʏᴏᴜʀ ᴍᴇssᴀɢᴇ ᴡᴀs ʜɪᴅᴅᴇɴ, ᴄʜᴀᴛ ᴜsᴇʀɴᴀᴍᴇs ɴᴏᴛ ᴀʟʟᴏᴡᴇᴅ ɪɴ ᴛʜɪs ɢʀᴏᴜᴘ."
-                        )
-                        await message.delete()
-                except:
-                    return
+        for link in links:
+            try:
+                user = await bot.get_chat(link)
+                print(user.id)
+                if len(str(user.id)) > 12:
+                    await message.reply_text(
+                        f"{users.first_name}, ʏᴏᴜʀ ᴍᴇssᴀɢᴇ ᴡᴀs ʜɪᴅᴅᴇɴ, ᴄʜᴀᴛ ᴜsᴇʀɴᴀᴍᴇs ɴᴏᴛ ᴀʟʟᴏᴡᴇᴅ ɪɴ ᴛʜɪs ɢʀᴏᴜᴘ."
+                    )
+                    await message.delete()
+            except Exception as e:
+                print(f"Error: {e}")
+                return
+
 
 
 USER = 110
